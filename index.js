be6ba0b94c3c780945f804bc83c9fc9682f29725
zahid-raza <intimidatorthe@gmail.com>
2024-10-07 10:14:23 +0530

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const connectDb = require("../backend/db/db");
 const userRouter = require("./router/userRouter");
 const expenseRouter = require("./router/expenseRouter");
